d160678ceacbd855d410581e3ddd4d3675912fdc
CopiVT <126109541+CopiVT@users.noreply.github.com>
2023-02-23 11:11:57 -0600

    Update streamlit_app.py
    introduced commands to make json readable

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+# Cleans up the json data to be readable 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# creates dataframe of the readable data
+streamlit.dataframe(fruityvice_normalized)
