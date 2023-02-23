aa8566db79a2339537c32a05f027b1366b41d7d8
CopiVT <126109541+CopiVT@users.noreply.github.com>
2023-02-23 13:38:32 -0600

    Update streamlit_app.py
    cleaned up imports and added new library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -9,7 +13,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -20,7 +23,6 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-import requests
 streamlit.header("Fruityvice Fruit Advice!")
 # add in a search bar
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
@@ -34,7 +36,6 @@ streamlit.dataframe(fruityvice_normalized)
 #time to troubleshoot
 streamlit.stop()
 
-import snowflake.connector
 #querying connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
